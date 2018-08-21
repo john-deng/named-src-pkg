0733ac70aacfab46e6860cc52faad161e26da08e
John Deng <john.deng@qq.com>
2018-08-21 23:26:54 +0800

    clean up

--- example/example.go
+++ example/example.go
@@ -1,6 +1,6 @@
 package example
 
-import "gitlab.vpclub.cn/demo/named-src-pkg/example/subpkg"
+import "hidevops.io/named-src-pkg/example/subpkg"
 
 func Greeting(name string) string  {
 	return "Hello " + name + " from " + subpkg.From()
